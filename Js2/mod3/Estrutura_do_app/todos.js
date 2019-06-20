e7354d4a253b44a3ce3f852f8e65a5b16a4ffda6
PajeAutista <vitor_cunha@estudante.sc.senai.br>
2019-06-20 02:08:48 -0300

    criando funçoes para adicionar listas

--- Js2/mod3/Estrutura_do_app/todos.js
+++ Js2/mod3/Estrutura_do_app/todos.js
@@ -10,6 +10,7 @@ var todos = [
 
 
 function renderTodos() {
+    listElement.innerHTML = "";
     for (todo of todos){
         var todoElement = document.createElement("li");
         var todoText = document.createTextNode(todo);
@@ -21,3 +22,12 @@ function renderTodos() {
 
 renderTodos();
 
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = "";
+    renderTodos();
+}
+
+buttonElement.onclick = addTodo;
