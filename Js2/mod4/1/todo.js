6fe4727ff34fc7ae50a9370aaab66103997eeadd
PajeAutista <vitor_cunha@estudante.sc.senai.br>
2019-06-20 03:05:03 -0300

    fiz a função para retornar a promise

--- Js2/mod4/1/todo.js
+++ Js2/mod4/1/todo.js
@@ -1,6 +1,13 @@
 function checaIdade(idade) {
-    // Retornar uma promise
-   }
+    return new Promise(function(resolve, rejeita) {
+      setTimeout(function() {
+        return idade >= 18 ? resolve() : rejeita();
+      }, 2000);
+    });
+  }
+
+
+
    checaIdade(20)
     .then(function() {
     console.log("Maior que 18");
