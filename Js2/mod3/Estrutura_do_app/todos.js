3e2ffb293794219826ecf71025cbdf6db2d7480d
PajeAutista <vitor_cunha@estudante.sc.senai.br>
2019-06-20 01:59:22 -0300

    mudando a lista para uma ray

--- Js2/mod3/Estrutura_do_app/todos.js
+++ Js2/mod3/Estrutura_do_app/todos.js
@@ -1,3 +1,11 @@
 var listElement = document.querySelector("#app ul")
 var inputElement = document.querySelector("#app input")
 var buttonElement = document.querySelector("#app button")
+
+var todos = [
+    "Fazer Trabalho"
+    "Estudar pra prova"
+    "Acessar o jogo"
+];
+
+
